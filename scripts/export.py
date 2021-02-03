c8b2afece15497f4eba2348c32c5d970686db543
Tamas Spisak <spisak.tms@gmail.com>
2021-02-03 14:10:06 +0100

    first implementation

---
+++ scripts/export.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import argparse
+import glob
+import os
+import json
+import pandas as pd
+from datetime import datetime
+
+
+parser = argparse.ArgumentParser(description='"Export SalivApp data."')
+parser.add_argument('data_dir', metavar='data_dir', type=str,
+                    help='directory containing the participant-specific sub-directories with data.json')
+parser.add_argument('-c', "--csv", dest="outcsv", action="store", default=None,
+                    help='Output csv file name name. (default: None)')
+parser.add_argument("-e", "--excel", dest="outexcel", action="store", default=None,
+                    help='Output Excel speadsheet file name name. (default: None)')
+
+args = parser.parse_args()
+
+if __name__ == '__main__':
+
+    # read all data
+    jsons = glob.glob( os.path.join(args.data_dir,'*/data.json'))
+
+    rows_list = []
+    for js in jsons:
+        with open(js, "r") as read_file:
+            participant_id = js.split('/')[-2]
+            data = json.load(read_file)
+
+
+            for d in data:
+                dt = datetime.fromtimestamp(d['timestamp'])
+                rows_list.append([participant_id, d['barcode'], d['timestamp'],
+                                  dt.date(), dt.time()])
+
+    df = pd.DataFrame(rows_list, columns=['participant_id', 'barcode_id', 'timestamp', 'date', 'time'])
+
+    print(df)
+
+    if args.outcsv:
+        df.to_csv(args.outcsv)
+
+    if args.outexcel:
+        df.to_excel(args.outexcel)
